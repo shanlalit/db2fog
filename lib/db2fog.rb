8f7ff42ecb511786ca4c1b43f2a74ea7af467014
Lalit Shandilya <lalit@soulcurry.co>
2021-08-12 19:58:30 +0530

    Updating use fog-core 2.2 and rails 6

--- lib/db2fog.rb
+++ lib/db2fog.rb
@@ -3,7 +3,7 @@
 require 'active_support'
 require 'active_support/core_ext/class/attribute_accessors'
 require 'active_support/core_ext/hash/except'
-require 'fog'
+require 'fog/core'
 require 'tempfile'
 require 'fileutils'
 require 'db2fog/railtie'
